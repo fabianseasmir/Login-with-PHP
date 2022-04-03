16cd4f7832cddbadee348616f0dae9cd2f50a501
Fabian Seas <fabiansm201@gmail.com>
2022-04-03 15:14:25 -0600

    Adding dev branch

--- indext.php
+++ indext.php
@@ -17,7 +17,7 @@ if(isset($_SESSION['usuario'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login and register</title>
+    <title>login and register to app</title>
  
    <link rel="stylesheet" href="./assets/css/estilos.css">
 
